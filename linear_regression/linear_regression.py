0348663159096553d206f7169a2d97bbf1681946
Ismaïl EL GOTE <160734505+diidouu@users.noreply.github.com>
2025-12-19 22:49:15 +0900

    Add gradient descent implementation and visualization for linear regression

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 
 m = 100
 x = np.linspace(0, 10 , m)
@@ -13,3 +14,27 @@ print(X.shape)  # Output: (100, 2)
 
 def h(X: np.ndarray, theta: np.ndarray) -> np.ndarray:
     return X @ theta
+
+def cost(X: np.ndarray, y: np.ndarray, theta: np.ndarray) -> float:
+    return 0.5 * (X @ theta -y).T @ (X @ theta - y)
+
+def gradient(X: np.ndarray, y: np.ndarray, theta: np.ndarray) -> np.ndarray:
+    return X.T @ (X @ theta - y)
+
+def gradient_descent(X: np.ndarray, y: np.ndarray, theta: np.ndarray, alpha: float, num_iters: int) -> np.ndarray:
+    losses = [] 
+    for t in range(num_iters):
+        theta = theta - alpha * gradient(X, y, theta)
+        losses.append(cost(X, y, theta))
+        if t % 10 == 0:
+            print('theta:', theta)
+    return theta, losses
+
+theta_init = np.zeros(2)
+theta_final, losses = gradient_descent(X, y, theta_init, alpha=0.0001, num_iters=1000)
+
+plt.plot(losses)
+plt.xlabel('Iteration')
+plt.ylabel('Cost')
+plt.title('Convergence of Gradient Descent')
+plt.show()
